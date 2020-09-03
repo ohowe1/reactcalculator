d2f5d4d204cc746b46674b8b8f66f5a5237e3cfe
Carelesshippo <ojhowe95@gmail.com>
2020-09-02 19:09:54 -0600

    Updates

---
+++ precache-manifest.65afbbfc65e4657c00e2e3ef089f8029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ac27cd5e108e9de7613a34e1469f73",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "15539ad8e8202d30fde2",
+    "url": "/reactcalculator/static/css/main.ad3b792d.chunk.css"
+  },
+  {
+    "revision": "4ffcba3a2e66cb80b5cf",
+    "url": "/reactcalculator/static/js/2.3cb2d42d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcalculator/static/js/2.3cb2d42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15539ad8e8202d30fde2",
+    "url": "/reactcalculator/static/js/main.1ceef373.chunk.js"
+  },
+  {
+    "revision": "ea3c41d40812c676113e",
+    "url": "/reactcalculator/static/js/runtime-main.43ced591.js"
+  }
+]);
